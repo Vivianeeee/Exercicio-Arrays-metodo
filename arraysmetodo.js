936aadacf2aabfff8bdb122d48de3991f0fb98cf
vivianeeee <vivianeoliveirandrade@gmail.com>
2021-11-10 08:43:46 -0300

    Arrays metodo > Hight

--- arraysmetodo.js
+++ arraysmetodo.js
@@ -1,59 +1,89 @@
 // Exercício
 
-const  funcionarios  =  [
-    {
-        id : 1 ,
-        nome : 'Davis' ,
-        salario : 2500 ,
-    } ,
-    {
-        id : 2 ,
-        nome : 'Breno' ,
-        salario : 3100 ,
-    } ,
-    {
-        id : 3 ,
-        nome : 'Karen' ,
-        salario : 3200 ,
-    } ,
-    {
-        id : 4 ,
-        nome : 'Camila' ,
-        salario : 2300 ,
-    } ,
-    {
-        id : 5 ,
-        nome : 'Julio' ,
-        salario : 1400 ,
-    } ,
-] ;
+/ ** O método percorre automaticamente os elementos do array.
+O método pode executar uma função sobre cada elemento da matriz. * /
 
+// Método forEach ()
+const  frutas  =  [ 'Uva' ,  'Pera' ,  'Caqui' ,  'Ameixa' ] ;
+frutas . forEach ( minhaFuncao ) ;
+function  minhaFuncao ( item ,  index )  {
+console . log ( `Índice: $ { index } ` ) ;
+console . log ( `Elemento: $ { item } ` ) ;
+}
 
+/ ** função seta O, é uma forma muito mais prática e que
+também torna o código mais enxuto e elegante. Utilizando
+o mesmo código anterior com função de seta:
+ * /
 
-funcionarios . forEach ( ( funcio )  =>  console . log ( `Funcionário: $ { funcio . nome } têm o sálario $ $ { funcio . salario } reais` ) ) ;
-// Reajuste
-console . log ( '' )
-deixe  newSalario  =  funcionarios . map ( ( novoSalario )  => {
-    deixe  reaju  =  novoSalario . salario  *  0,05 ;
-
-      voltar  reaju  +  novoSalario . salario ;
-
+// Método forEach () com função de seta
+console . limpar ( ) ;
+const  frutas01  =  [ 'Uva' ,  'Pera' ,  'Caqui' ] ;
+frutas . forEach ( ( índice ,  item )  =>  {
+console . log ( `Índice: $ { index } ` ) ;
+console . log ( `Elemento: $ { item } ` ) ;
 } ) ;
 
-console . log ( `Com o reajuste: $ { newSalario } ` )
-console . log ( '' )
+// forEach () com array de objetos
+console . limpar ( ) ;
+ tarefas  constantes =  [
+{
+id : 1 ,
+texto : 'Levar o lixo para fora' ,
+isCompleted : false ,
+} ,
+{
+id : 2 ,
+texto : 'Encontrar com o chefe' ,
+isCompleted : true ,
+} ,
+{
+id : 3 ,
+texto : 'Consulta no dentista' ,
+isCompleted : false ,
+} ,
+] ;
+tarefas . forEach ( ( teste )  =>  console . log ( teste . texto ) ) ;
 
-// Recebem mais de 5000
-let  filtroSalario  =  newSalario . filtro ( ( salari )  => {
-    retorno  salari  > =  7000 ;
-} )
+/ ** O map () executa uma vez função para cada elemento de array que
+possui um valor atribuído e constrói um novo array com base nos
+valores retornados pela execução da função * /
 
-console . log ( `O salário mais alto: $ { filtroSalario } ` ) ;
-console . log ( '' )
+// Método map ()
+const  numeros  =  [ 5 ,  7 ,  18 ,  28 ] ;
+const  newArray  =  numeros . mapa ( Math . sqrt ) ;  //Math.sqrt (Raiz quadrada)
+console . limpar ( ) ;
+console . log ( 'Números:' ) ;
+console . log ( numeros ) ;
+console . log ( 'Novo array:' ) ;
+console . log ( newArray ) ;
 
+/ ** O filtro () executa uma vez uma função para cada elemento de array que
+possui um valor atribuído e constrói um novo array com os
+elementos que retornarem true para uma condição no filtro.
+ * /
+// Filtro de método
+console . limpar ( ) ;
+const  idades  =  [ 38 ,  35 ,  18 ,  47 ] ;
+let  filtroIdade  =  idades . filtro ( ( idade )  =>  {
+voltar  idade  > =  19 ;
+} ) ;
+console . log ( filtroIdade ) ;
 
-// Pesquisar o funcionário Wally
-console . log ( funcionarios . find ( ( funcionario )  => {
-    console . log ( 'Dados do funcionario:' )
-    retorno  funcionario . nome  ===  'Davis' ;
-} ) ) ; 
+/ ** o find () executa uma função vez a para cada elemento de array e
+retorna os valores que retornarem true para a condição configurada
+sem filtro.
+ * /
+
+// Método find ()
+console . limpar ( ) ;
+const  meuArray  =  [
+{ nome : 'Uva' ,  quantidade : 2  } , 
+{ nome : 'Acerola' ,  quantidade : 0  } ,
+{ nome : 'Pessego' ,  quantidade : 5  } , 
+] ;
+
+console . log (
+meuArray . find ( ( fruta )  =>  {
+return  fruta . nome  ===  'Pessego' ;
+} )  ) ; 
